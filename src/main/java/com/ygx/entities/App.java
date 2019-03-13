8c8a49e5640454e0d7debd8def3f4c0d3b896a41
yuanguoxian <910010208@qq.com>
2019-03-13 16:42:44 +0800

    update

--- src/main/java/com/ygx/entities/App.java
+++ src/main/java/com/ygx/entities/App.java
@@ -2,7 +2,7 @@ package com.ygx.entities;
 
 public class App {
 
-	 private String name="hello world! Hello Everyone!";
+	 private String name="hello world! Hello Everyone!uoiuoiu";
 	 public String getName() {
 		return name;
 	}
